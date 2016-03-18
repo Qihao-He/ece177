57dafbe6afdeb1f78aba3be6dcce9b0347713b7d
Qihao_ubuntu <qi.he@maine.edu>
2016-03-18 10:55:51 -0400

    This is problem 4 solved

--- lab1.c
+++ lab1.c
@@ -1,4 +1,3 @@
-/*this is the main function of the lab2 of ece177*/
 #include <stdio.h>
 #include <math.h>
 /*Question 1 is translate temperature from Fahrenheit to Celsius.*/
@@ -19,17 +18,29 @@ int polynomial(){
 }
 /*Question 3 is to calculate two solutions of the equation ax^2+bx+c=0*/
 int solution(){
-	double a,b,c,x1,x2;
+	int a,b,c;
+	double x1,x2;
 	printf("Please enter the a,b,c values by sequence:\n");
-	scanf("%lf%lf%lf",a,b,c);
-	x1=(-b+sqr(b*b-4*a*c))/(2*a);
-	x2=(-b-sqr(b*b-4*a*c))/(2*a);
-	printf("The solution\nx1 is: %lf\nx2 is:\n",x1,x2);
+	scanf("%d%d%d",&a,&b,&c);
+	x1=(-b+sqrt(b*b-4*a*c))/(2*a);
+	x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+	printf("The solution\nx1 is: %f\nx2 is: %f\n",x1,x2);
 }
 /*Question 4 is to compute the distance s fallen by an object in free fall. The formula is s=s0+v0*t+(1/2)*a*t^2, s0 is the initial position in feet, v0 is the initial downward velovity in ft/sec, t is the time in seconds, and a is 32.2ft/sec^2. The output values are s and t where t=0,5,10...100.*/
 int freefalldistance(){
-	int t;
-	double s0,v0,
+	int t[20],s[20],i,j;
+	int s0,v0;
+	double a;
+	a=32.2;
+	printf("Please input values of s0 and v0:\n");
+	scanf("%d%d",&s0,&v0);
+	for(i=0;i<20;i++){
+		t[i]=(i+1)*5;
+	}
+	for(j=0;j<20;j++){
+		s[j]=s0+v0*t[j]+(1/2)*a*t[j]*t[j];
+		printf("The distance of freefall at time %d is:%d\n",t[j],s[j]);
+	}	
 }
 int main(){
 int question;
@@ -47,7 +58,10 @@ int question;
 		break;
 	case 4:
 		freefalldistance();
-		break;		
+		break;
+	default:
+		printf("Sorry, your choice did not match any of the four. Please try again.\n");
+		break;
 	}
 return 0;
 }
