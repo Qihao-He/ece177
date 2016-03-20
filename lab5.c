f9ca4db04a90f7212fc0aa5ebaa4c220f4faac4c
Qihao_ubuntu <qi.he@maine.edu>
2016-03-20 12:50:47 -0400

    This is half done in C level.

--- lab5.c
+++ lab5.c
@@ -1,12 +1,48 @@
 /*This is ece177 lab5.*/
 #include<stdio.h>
+void c(){
+	int i,j,k,h,m,s;
+	printf("This is c level.\n");
+	printf("please input the hour: the minute: the second: \n");
+	scanf("%d%d%d",&h,&m,&s);
+	for(i=0;i<=2;i++){
+		if(i==0){
+		j=h;	
+		}
+		if(i==1){
+		j=m;
+		}
+		if(i==2){
+		j=s;
+		}
+		for(k=0;k<j;k++){
+			printf("*");
+		}
+		printf("\n");
+	} 
+} 
+void b(){
+	printf("This is b level.\n");
+	printf("please input the hour: the minute: the second: \n");
+//	scanf("%d%d%d",&h,&m,&s);
+
+}
+void a(){
+	printf("This is a level.\n");
+	printf("please input the hour: the minute: the second: \n");
+//	scanf("%d%d%d",&h,&m,&s);
+
+}
+
 int main() {
+	c();
+	b();
+	a();
+/*
 	int h, m, s, i, j, k;
 	h = 0;
 	m = 0;
 	s = 0;
-	printf("please input the hour: the minute: the second: \n");
-	scanf_s("%d%d%d",&h,&m,&s);
 	for (j = 0; j <= 2; j++) {
 		if (j = 0) {
 			k = h;
@@ -18,11 +54,9 @@ int main() {
 			k = s;
 		}
 		for{
-
 		}
 	}
 	while ((h!= 0 )|| (m!= 0) || (s!= 0)) {
-		/*hour and with 6 bit shifting 1 bit*/
 		for (i = 32; i != 0; i>>= 1) {
 			if (i&h) {
 				printf("*");
@@ -33,5 +67,6 @@ int main() {
 		printf("\n");
 	
 	}
-	return 0;
+*/
+return 0;
 }
