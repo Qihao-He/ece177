d28d600cf88ec6f3b4b94445403df8ea562e1da0
Qihao_ubuntu <qi.he@maine.edu>
2016-03-28 12:58:46 -0400

    Function exercise 1.

---
+++ functionexercise1.c
@@ -0,0 +1,20 @@
+/*Write a function called print_big which receives one argument (an integer) and prints the line x is big if the argument given to the function is a number bigger than 10.*/
+
+#include <stdio.h>
+
+/* function declaration */
+int print_big(int number);
+
+int main() {
+  int array[] = { 1, 11, 2, 22, 3, 33 };
+  int i;
+  for (i = 0; i < 6; i++) {
+    print_big(array[i]\n);
+  }
+  return 0;
+}
+
+/* write your function here */
+int print_big(int number){
+
+}
